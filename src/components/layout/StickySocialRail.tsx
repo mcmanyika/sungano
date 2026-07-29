b072e12f5ebec60b7bc98050a6a145bc8965a107
mcmanyika <mcmanyika@gmail.com>
2026-07-29 11:39:36 -0500

    update

--- src/components/layout/StickySocialRail.tsx
+++ src/components/layout/StickySocialRail.tsx
@@ -12,7 +12,7 @@ export function StickySocialRail() {
   const { navigation } = useSiteNavigation();
 
   return (
-    <div className="pointer-events-none absolute inset-y-0 left-3 z-40 hidden w-11 pt-[max(7.5rem,calc(50svh-7rem))] lg:left-8 lg:block lg:w-12">
+    <div className="pointer-events-none absolute inset-y-0 left-8 z-40 hidden w-12 pt-[max(7.5rem,calc(50svh-7rem))] min-[1200px]:block">
       <motion.div
         variants={fadeUp}
         initial="hidden"
